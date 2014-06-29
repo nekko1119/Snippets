3dd6d4025a5ddfbdd1e5d611af7e1ccf4fb82654
nekko1119 <taku50501119@gmail.com>
2014-06-30 00:39:01 +0900

    細かいところを修正

--- knapsack.cpp
+++ knapsack.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <iostream>
 
+// 品物
 struct good {
   int size;
   int value;
@@ -20,8 +21,7 @@ int main()
   constexpr int knap_max_size = 16;
   std::array<int, knap_max_size + 1> knapsack = {{}};
   std::cout << "ナップザックの大きさ:\t";
-  for (int i = 1; i < knap_max_size + 1; ++i)
-  {
+  for (int i = 1; i < knap_max_size + 1; ++i) {
     std::cout << std::setw(2) << i << " ";
   }
   std::cout << "\n\n";
@@ -29,6 +29,8 @@ int main()
   for (int i = 0; i < static_cast<int>(goods.size()); ++i) {
     // ナップザックの大きさ
     for (int knap_size = goods[i].size;  knap_size < knap_max_size + 1; ++knap_size) {
+      // 今までの価値から現在の品物を入れるために品物分のサイズを引いた価値に、
+      // 実際に追加してみて、価値が更新したら本当に代入する
       int new_value = knapsack[knap_size - goods[i].size] + goods[i].value;
       if (knapsack[knap_size] < new_value) {
         knapsack[knap_size] = new_value;
@@ -41,3 +43,13 @@ int main()
     std::cout << "\n";
   }
 }
+/*
+ナップザックの大きさ:  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 
+
+品物1まで使う:     0  2  2  4  4  6  6  8  8 10 10 12 12 14 14 16 
+品物2まで使う:     0  2  4  4  6  8  8 10 12 12 14 16 16 18 20 20 
+品物3まで使う:     0  2  4  4  7  8  9 11 12 14 15 16 18 19 21 22 
+品物4まで使う:     0  2  4  4  7 10 10 12 14 14 17 20 20 22 24 24 
+品物5まで使う:     0  2  4  4  7 10 10 12 16 16 18 20 20 23 26 26 
+[Finished in 0.1s]
+*/
